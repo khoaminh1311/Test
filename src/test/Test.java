eea964a35c532b9dba62834e80d9fa05b258c933
MInh Khoa <124848425+minhkhoa131103@users.noreply.github.com>
2024-10-28 23:42:22 +0700

    Commit thêm phần scanner

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,15 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package test;
 
+package test;
+import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
-       System.out.println("Hello World");
-       System.out.println("Tra Van Minh Khoa");
-       System.out.println("CMU-CS 303 AGIS");
-       System.out.println("2921905467");
+       Scanner keyboard = new Scanner(System.in);
+      // nhập họ và tên
+       System.out.print("Nhap ho va ten: ");
+       String name = keyboard.nextLine();
+       System.out.println("Ho va ten: " + name);
+      //nhập mssv
+       System.out.print("Nhap MSSV: ");
+       long num = keyboard.nextLong();
+       keyboard.nextLine();
+       System.out.println("MSSV: " + num);
+//       nhập tên lớp
+       System.out.print("Nhap ten lop: ");
+       String c = keyboard.nextLine();
+       System.out.println("Ma lop: " + c);
+//       System.out.println("Hello World");
+//       System.out.println("Tra Van Minh Khoa");
+//       System.out.println("CMU-CS 303 AGIS");
+//       System.out.println("2921905467");
     }
     
 }
