93c5fc90d5a6a5db6e7febfb885bfc7607534349
MInh Khoa <124848425+minhkhoa131103@users.noreply.github.com>
2024-10-28 22:38:58 +0700

    First Commit

---
+++ src/test/Test.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+       System.out.println("Hello World");
+       System.out.println("Tra Van Minh Khoa");
+       System.out.println("CMU-CS 303 AGIS");
+       System.out.println("2921905467");
+    }
+    
+}
